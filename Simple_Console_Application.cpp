1eb8a93b2e024100510abd8820137f6e6bb951a1
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2023-05-24 20:40:51 +0200

    Add files via upload

---
+++ Simple_Console_Application.cpp
@@ -0,0 +1,107 @@
+// Import libraries
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+// ==================== VARIABLE DECLARATIONS ====================
+
+/*
+
+int = whole number    int num = 5;
+double = decimal      double num = 5.5;
+char = single character     char letter = 'a'; )
+string = text       string text = " Hollow ";
+bool = states       bool Boolean = true;  { true = 1    false = 0 }
+
+*/
+
+// ==================== ARITHMETIC OPERATORS ====================
+
+/*
+
+=   is      x=5
+==  Equal to	x == y
+!=	Not equal	x != y
+>	Greater than	x > y
+<	Less than	x < y
+>=	Greater than or equal to	x >= y
+<=	Less than or equal to	x <= y
+
+*/
+
+// ==================== WHILE LOOP HOW TO ====================
+
+/*
+
+    int i = 0;
+
+    while ( i < 5 ) 
+    {
+        cout << i << "\n";
+        i++;
+    }
+
+*/
+
+// ==================== FOR LOOP HOW TO ====================
+
+/*
+
+    for ( int i = 0; i < 5; i++ )
+    {
+        cout << i << "\n";
+    }
+
+*/
+
+// ==================== SWITCH CASE HOW TO ====================
+
+/*
+
+    int day = 4;
+
+    switch (day)
+    {
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        case 7:
+            cout << "Sunday";
+            break;
+        default:
+            cout << "Invalid";
+    }
+
+*/
+
+// ==================== CREATE AN ARRAY ====================
+
+/*
+
+    string cars[4] = { "VW", "VW", "VW", "VW" };
+
+*/
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World! \n";
+}
