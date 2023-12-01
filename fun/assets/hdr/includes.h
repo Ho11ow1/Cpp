e5ec64d34b0cfbed4edfe1843ea8f6eaa6c8cd3f
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2023-12-01 16:13:24 +0100

    Create includes.h

---
+++ fun/assets/hdr/includes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+
+#endif
