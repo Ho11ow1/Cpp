be4dc1f9cdad63736ef06be1701730c2cea450c6
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2024-02-11 21:32:09 +0100

    Update main.h

--- src/include/main.h
+++ src/include/main.h
@@ -3,17 +3,18 @@
 #ifndef main_H
 #define main_H
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <fstream>
+#include <iostream> // Main function
+#include <stdio.h> // Std C function
+#include <stdlib.h> // More C functions
+#include <windows.h> // Windows manipulation functions
+#include <conio.h> // System functions
+#include <fstream> // File managment
 
-#include <string>
-#include <vector>
-#include <map>
-#include <iterator>
+#include <string> // String function
+#include <vector> // Vector as Array
+#include <unordered_map> // Map & sorting
+#include <iterator> // Iterate over things
 
-#include <cmath>
+#include <cmath> // More complex math
 
 #endif
