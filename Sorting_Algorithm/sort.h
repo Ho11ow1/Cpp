cd7710dec47789dc6df680ac043225cfc896dfd3
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2024-05-21 00:15:57 +0200

    Update sort.h

--- Sorting_Algorithm/sort.h
+++ Sorting_Algorithm/sort.h
@@ -4,162 +4,164 @@
 
 #include <vector>
 
-namespace vector_sort
+namespace Hollow
 {
-    std::vector<uint32_t> sort(std::vector<uint32_t>& vec)
+    namespace vector_sort
     {
-        for (int i = 0; i < vec.size(); i++)
+        std::vector<uint32_t> sort(std::vector<uint32_t>& vec)
         {
-            for (int j = i + 1; j < vec.size(); j++)
+            for (int i = 0; i < vec.size(); i++)
             {
-                if (vec[i] > vec[j])
+                for (int j = i + 1; j < vec.size(); j++)
                 {
-                    std::swap(vec[i], vec[j]);
+                    if (vec[i] > vec[j])
+                    {
+                        std::swap(vec[i], vec[j]);
+                    }
                 }
             }
+
+            return vec;
         }
 
-        return vec;
-    }
-
-    std::vector<int> sort(std::vector<int>& vec)
-    {
-        for (int i = 0; i < vec.size(); i++)
+        std::vector<int> sort(std::vector<int>& vec)
         {
-            for (int j = i + 1; j < vec.size(); j++)
+            for (int i = 0; i < vec.size(); i++)
             {
-                if (vec[i] > vec[j])
+                for (int j = i + 1; j < vec.size(); j++)
                 {
-                    std::swap(vec[i], vec[j]);
+                    if (vec[i] > vec[j])
+                    {
+                        std::swap(vec[i], vec[j]);
+                    }
                 }
             }
+
+            return vec;
         }
 
-        return vec;
-    }
-
-    std::vector<double> sort(std::vector<double>& vec)
-    {
-        for (int i = 0; i < vec.size(); i++)
+        std::vector<double> sort(std::vector<double>& vec)
         {
-            for (int j = i + 1; j < vec.size(); j++)
+            for (int i = 0; i < vec.size(); i++)
             {
-                if (vec[i] > vec[j])
+                for (int j = i + 1; j < vec.size(); j++)
                 {
-                    std::swap(vec[i], vec[j]);
+                    if (vec[i] > vec[j])
+                    {
+                        std::swap(vec[i], vec[j]);
+                    }
                 }
             }
+
+            return vec;
         }
 
-        return vec;
-    }
-
-    std::vector<char> sort(std::vector<char>& vec)
-    {
-        for (int i = 0; i < vec.size(); i++)
+        std::vector<char> sort(std::vector<char>& vec)
         {
-            for (int j = i + 1; j < vec.size(); j++)
+            for (int i = 0; i < vec.size(); i++)
             {
-                if (vec[i] > vec[j])
+                for (int j = i + 1; j < vec.size(); j++)
                 {
-                    std::swap(vec[i], vec[j]);
+                    if (vec[i] > vec[j])
+                    {
+                        std::swap(vec[i], vec[j]);
+                    }
                 }
             }
+
+            return vec;
+        }
+    }
+    // Example for vector_sort
+    /*void vector()
+    {
+        std::vector<uint32_t> nums = { 1, 7, 3, 8, 2 };
+
+        std::vector<uint32_t> sorted = Hollow::vector_sort::sort(nums);
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            printf("%d%s", sorted[i], " ");
+        }
+    */
+
+    namespace type_sort
+    {
+        uint32_t* sort(uint32_t arr[], int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        std::swap(arr[i], arr[j]);
+                    }
+                }
+            }
+            return arr;
         }
 
-        return vec;
+        int* sort(int arr[], int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        std::swap(arr[i], arr[j]);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        double* sort(double arr[], int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        std::swap(arr[i], arr[j]);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        char* sort(char arr[], int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        std::swap(arr[i], arr[j]);
+                    }
+                }
+            }
+            return arr;
+        }
     }
+    // Example for type_sort
+    /*void type()
+    {
+        char arr[] = { 'a', 'c', 'f', 'b' };
+
+        int size = sizeof(arr) / sizeof(arr[0]);
+
+        Hollow::type_sort::sort(arr, size);
+
+        for (int i = 0; i < size; i++)
+        {
+            printf("%c%s", arr[i], " ");
+        }
+    }
+    */
 }
-// Example for vector_sort
-/*void vector()
-{
-    std::vector<uint32_t> nums = { 1, 7, 3, 8, 2 };
-
-    std::vector<uint32_t> sorted = vector_sort::sort(nums);
-
-    for (int i = 0; i < nums.size(); i++)
-    {
-        printf("%d%s", sorted[i], " ");
-    }
-*/
-
-namespace type_sort
-{
-    uint32_t* sort(uint32_t arr[], int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i + 1; j < size; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    std::swap(arr[i], arr[j]);
-                }
-            }
-        }
-        return arr;
-    }
-
-    int* sort(int arr[], int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i + 1; j < size; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    std::swap(arr[i], arr[j]);
-                }
-            }
-        }
-        return arr;
-    }
-
-    double* sort(double arr[], int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i + 1; j < size; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    std::swap(arr[i], arr[j]);
-                }
-            }
-        }
-        return arr;
-    }
-
-    char* sort(char arr[], int size)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = i + 1; j < size; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    std::swap(arr[i], arr[j]);
-                }
-            }
-        }
-        return arr;
-    }
-}
-
-// Example for type_sort
-/*void type()
-{
-    char arr[] = { 'a', 'c', 'f', 'b' };
-
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    type_sort::sort(arr, size);
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%c%s", arr[i], " ");
-    }
-}
-*/
 
 #endif
