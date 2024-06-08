3bae97fec74bf664a037ee756db621e56c57df35
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2024-06-08 02:59:56 +0200

    Create BJ.h

---
+++ Black_Jack/src/Include/BJ.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef BJ_H
+#define BJ_H
+
+#include <vector>
+#include <memory>
+#include <random>
+
+using std::make_unique;
+using std::unique_ptr;
+using std::vector;
+
+#endif 
